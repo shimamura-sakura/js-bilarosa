feb1aeb25ceda2fd742f5af1e39736e07175006b
Shimamura Sakura <shimamura_sakura@pm.me>
2024-02-27 22:39:36 +0800

    support mimiwuhui

---
+++ 3-mimiwuhui.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { runForGameEte } = require('./0-common.js');
+
+runForGameEte('index.html', '秘密舞会', [
+    'ch01.ks',
+    'ch02.ks',
+    'ch03.ks',
+    'ch04.ks',
+    'ch05.ks',
+    'ch06.ks',
+]);
